e60944eecce5b9ccb5427bcf3f0cfdb1581a49dd
Chuanqi Zhang <zhang.chua@northeastern.edu>
2020-10-28 22:02:16 -0400

    Update 209_using_sliding_window.java

--- 209_using_sliding_window.java
+++ 209_using_sliding_window.java
@@ -15,3 +15,30 @@ class Solution {
         return 0;
     }
 }
+
+
+// better window sliding solution
+// https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        int windowStart = 0;
+        int windowEnd = 0;
+        int windowSum = 0;
+        int min = nums.length + 1;
+        
+        for (; windowEnd < nums.length; windowEnd++) {
+            windowSum += nums[windowEnd];
+            
+            while (windowSum >= s) {
+                min = Math.min(min, windowEnd - windowStart + 1);
+                windowSum -= nums[windowStart];
+                windowStart++;
+            }
+        }
+        
+        if (min == nums.length + 1) min = 0;
+        return min;
+    }
+}
