342a8b7caad54c7d1dab1ecc54f9528337a93f1a
Chuanqi Zhang <zhang.chua@northeastern.edu>
2020-11-01 18:54:07 -0500

    Create 145_Binary_Tree_Postorder_Traversal.java

---
+++ 145_Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,30 @@
+// iteration
+// a node is visited only when it is popped from the stack and it has no right child
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        
+        TreeNode curr = root;
+        Stack<TreeNode> stack = new Stack<>();
+        
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                if (curr.right != null) stack.push(curr.right);
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                if (!stack.isEmpty() && curr.right == stack.peek()) {
+                    TreeNode temp = curr;
+                    curr = stack.pop();
+                    stack.push(temp);
+                } else {
+                    result.add(curr.val);
+                    curr = null;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
