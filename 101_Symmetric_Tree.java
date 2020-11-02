dd223c8e8e7eb6dff8dc09280197c2faa398be50
Chuanqi Zhang <zhang.chua@northeastern.edu>
2020-11-01 19:45:45 -0500

    Create 101_Symmetric_Tree.java

---
+++ 101_Symmetric_Tree.java
@@ -0,0 +1,30 @@
+// iteration
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        if (root.left == null || root.right == null) return root.left == root.right;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode n1 = queue.poll();
+            TreeNode n2 = queue.poll();
+            if (n1.val != n2.val) return false;
+            if (n1.left != null && n2.right != null) {
+                queue.offer(n1.left);
+                queue.offer(n2.right);
+            } else if (n1.left != n2.right) {
+                return false;
+            }
+            if (n1.right != null && n2.left != null) {
+                queue.offer(n1.right);
+                queue.offer(n2.left);
+            } else if (n1.right != n2.left) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
