8dac6527df161ab35581f7759f35d946cccc23ba
Chuanqi Zhang <zhang.chua@northeastern.edu>
2020-11-01 22:43:11 -0500

    Create 1_Two_Sum.java

---
+++ 1_Two_Sum.java
@@ -0,0 +1,19 @@
+// use a hashtap to store the position of each data
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        
+        return result;
+    }
+}
