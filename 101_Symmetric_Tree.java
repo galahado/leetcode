69fe6fab50baec4826ccf748223ad16ac001cc9d
Chuanqi Zhang <zhang.chua@northeastern.edu>
2020-11-02 08:04:28 -0500

    Update 101_Symmetric_Tree.java

--- 101_Symmetric_Tree.java
+++ 101_Symmetric_Tree.java
@@ -1,3 +1,18 @@
+// recursion
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null || right == null) return left == right;
+        else if (!(left.val == right.val)) return false;
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
+
+
 // iteration
 class Solution {
     public boolean isSymmetric(TreeNode root) {
