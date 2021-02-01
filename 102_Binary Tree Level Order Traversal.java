1cd29a7ce293e70580ace2fcfcfe567396ec50d5
Chuanqi Zhang <zhang.chua@northeastern.edu>
2021-01-31 20:26:10 -0500

    102

--- 102_Binary Tree Level Order Traversal.java
+++ 102_Binary Tree Level Order Traversal.java
@@ -2,8 +2,9 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-        
+        if (root == null)
+            return result;
+
         List<TreeNode> up = new ArrayList<>();
         List<TreeNode> down = new ArrayList<>();
         up.add(root);
@@ -11,18 +12,19 @@ class Solution {
             List<Integer> list = new ArrayList<>();
             for (TreeNode node : up) {
                 list.add(node.val);
-                if (node.left != null) down.add(node.left);
-                if (node.right != null) down.add(node.right);
+                if (node.left != null)
+                    down.add(node.left);
+                if (node.right != null)
+                    down.add(node.right);
             }
             result.add(list);
             up = down;
             down = new ArrayList<>();
         }
-        
+
         return result;
     }
 }
-
 
 // recursion
 class Solution {
@@ -35,18 +37,49 @@ class Solution {
         level.add(root);
         return helper(level);
     }
-    
+
     private List<List<Integer>> helper(List<TreeNode> nodes) {
         List<List<Integer>> result = new LinkedList<>();
         List<Integer> list = new LinkedList<>();
         List<TreeNode> nextLevel = new LinkedList<>();
         for (TreeNode node : nodes) {
             list.add(node.val);
-            if (node.left != null) nextLevel.add(node.left);
-            if (node.right != null) nextLevel.add(node.right);
+            if (node.left != null)
+                nextLevel.add(node.left);
+            if (node.right != null)
+                nextLevel.add(node.right);
         }
         result.add(list);
-        if (nextLevel.size() > 0) result.addAll(helper(nextLevel));
+        if (nextLevel.size() > 0)
+            result.addAll(helper(nextLevel));
+        return result;
+    }
+}
+
+// with BFS
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> list = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                list.add(node.val);
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+            result.add(list);
+        }
+
         return result;
     }
 }
